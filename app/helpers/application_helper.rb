3480f33cf538b77364200955578e65ca7dfc0096
Armando Amador <arm.amador@gmail.com>
2014-12-10 18:29:16 -0500

    Add Navigation bar and create homepage  - Move jquery date picker code to a partial  - Take out css sprockets and use @import instead  - Create Utilities scss  - Create Mobile, top, side, nav partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def class_for_main_content
+    return "large-10 medium-10 columns" if user_signed_in?
+    "medium-6 medium-offset-3 small-10 small-offset-1 columns"
+  end
 end
