ef1356bd420bbdda8a488575485e7ae2bd00dc1a
andygimma <andy.n.gimma@gmail.com>
2015-01-29 14:42:03 -0500

    Show active link on side nav  - is_active? helper  - check controller and link_path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     return "large-10 medium-12 small-12 columns light-grey-bg-pattern" if user_signed_in?
     "medium-6 medium-offset-3 small-10 small-offset-1 columns light-grey-bg-pattern"
   end
+
+  def active?(link_path)
+    return "active" if link_path == "/" && controller.controller_name == "static_pages"
+    return "active" if link_path.include?(controller.controller_name)
+  end
 end
