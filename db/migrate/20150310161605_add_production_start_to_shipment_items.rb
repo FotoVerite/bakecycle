b5c94cf3667538206f801481748a8cb5673e89b1
Francis Gulotta <wizard@roborooter.com>
2015-03-10 18:59:08 -0400

    Fix migration for shipment#production_start and font urls
     - Migration was failing if there was data in the database

--- db/migrate/20150310161605_add_production_start_to_shipment_items.rb
+++ db/migrate/20150310161605_add_production_start_to_shipment_items.rb
@@ -1,5 +1,6 @@
 class AddProductionStartToShipmentItems < ActiveRecord::Migration
   def change
-    add_column :shipment_items, :production_start, :date, null: false
+    add_column :shipment_items, :production_start, :date, null: false, default: Date.today
+    change_column_default(:shipment_items, :production_start, nil)
   end
 end
