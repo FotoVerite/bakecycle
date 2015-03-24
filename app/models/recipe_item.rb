e4d00b1cb3fcb17335332f1abd2e967cb8553a8b
Amy Gimma <amyg225@gmail.com>
2015-03-24 18:13:21 -0400

    recipe total lead days

--- app/models/recipe_item.rb
+++ app/models/recipe_item.rb
@@ -5,6 +5,14 @@ class RecipeItem < ActiveRecord::Base
   validates :inclusionable_id_type, presence: true
   validates :bakers_percentage, format: { with: /\A\d+(?:\.\d{0,3})?\z/ }, numericality: { greater_than: 0.001 }
 
+  delegate :total_lead_days, to: :inclusionable, allow_nil: true
+
+  scope :recipes, -> { where(inclusionable_type: 'Recipe') }
+
+  def self.max_lead_days
+    recipes.map(&:total_lead_days).max || 0
+  end
+
   def inclusionable_id_type=(composit_id)
     self.inclusionable_id, self.inclusionable_type = composit_id.split('-')
   end
