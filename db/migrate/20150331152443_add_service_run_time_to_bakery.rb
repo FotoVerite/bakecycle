3a120d4afa76526fc63f0cb5ad4f7cb02270ad37
Armando Amador <arm.amador@gmail.com>
2015-04-01 15:14:21 -0400

    Fix kickoff_time database bug

--- db/migrate/20150331152443_add_service_run_time_to_bakery.rb
+++ db/migrate/20150331152443_add_service_run_time_to_bakery.rb
@@ -1,6 +1,8 @@
 class AddServiceRunTimeToBakery < ActiveRecord::Migration
   def change
-    add_column :bakeries, :kickoff_time, :time, null: false
+    add_column :bakeries, :kickoff_time, :time
+    Bakery.update_all(kickoff_time: Chronic.parse('2 pm'))
     add_column :bakeries, :last_kickoff, :datetime
+    change_column :bakeries, :kickoff_time, :time, null: false
   end
 end
