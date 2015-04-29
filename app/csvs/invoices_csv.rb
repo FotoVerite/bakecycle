239976172bf8800f2233dadce68c409b82a9bb31
Armando Amador <arm.amador@gmail.com>
2015-04-29 16:25:20 -0400

    export invoice totals as a csv

---
+++ app/csvs/invoices_csv.rb
@@ -0,0 +1,32 @@
+class InvoicesCsv
+  attr_reader :shipments
+
+  def initialize(shipments)
+    @shipments = shipments
+  end
+
+  def generate
+    CSV.generate do |csv|
+      csv << header
+      @shipments.each do |shipment|
+        csv << shipment_data_row(shipment)
+      end
+    end
+  end
+
+  private
+
+  def header
+    [
+      'Invoice Number', 'Client Name', 'Client ID',
+      'Invoice Date', 'Invoice Subtotal', 'Invoice fees', 'Invoice total'
+    ]
+  end
+
+  def shipment_data_row(shipment)
+    [
+      shipment.invoice_number, shipment.client_name, shipment.client_id,
+      shipment.date, shipment.subtotal, shipment.delivery_fee, shipment.price
+    ]
+  end
+end
---
+++ spec/csvs/invoices_csv_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe InvoicesCsv do
+  it 'renders csv invoices from a collection of shipments' do
+    bakery = build_stubbed(:bakery)
+    client = build_stubbed(:client, name: 'Mando', bakery: bakery)
+    route = build_stubbed(:route, bakery: bakery)
+    shipment = build_stubbed(:shipment, client: client, bakery: bakery, route: route)
+    csv = InvoicesCsv.new([shipment.decorate]).generate
+    expect(csv).to include('Mando')
+    expect(csv).to include(shipment.invoice_number)
+  end
+end
