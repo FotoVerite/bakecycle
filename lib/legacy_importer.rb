609498b64357d42cf5de39e1915003f07c683126
Francis Gulotta <wizard@roborooter.com>
2015-05-03 20:06:35 -0400

    Stick a beta on the production run projections

--- lib/legacy_importer.rb
+++ lib/legacy_importer.rb
@@ -1,3 +1,5 @@
+# rubocop:disable Rails/Output
+
 require 'legacy_importer/models'
 require 'legacy_importer/importer'
 require 'legacy_importer/reporter'
@@ -21,91 +23,91 @@ module LegacyImporter
   end
 
   def self.import_clients
-    Rails.logger.warn 'Importing Clients'
+    puts 'Importing Clients'
     Importer.new(
       bakery: bakery,
       collection: Clients.all,
       importer: ClientImporter
     )
       .import!
-      .tap { |c| Rails.logger.warn "Imported #{c.count} clients" }
+      .tap { |c| puts "Imported #{c.count} clients" }
   end
 
   def self.import_ingredients
-    Rails.logger.warn 'Importing Ingredients'
+    puts 'Importing Ingredients'
     Importer.new(
       bakery: bakery,
       collection: Ingredients.all,
       importer: IngredientImporter
     )
       .import!
-      .tap { |c| Rails.logger.warn "Imported #{c.count} Ingredients" }
+      .tap { |c| puts "Imported #{c.count} Ingredients" }
   end
 
   def self.import_recipes
-    Rails.logger.warn 'Importing Recipes'
+    puts 'Importing Recipes'
     Importer.new(
       bakery: bakery,
       collection: Recipes.all,
       importer: RecipeImporter
     )
       .import!
-      .tap { |c| Rails.logger.warn "Imported #{c.count} Recipes" }
+      .tap { |c| puts "Imported #{c.count} Recipes" }
   end
 
   def self.import_recipe_items
-    Rails.logger.warn 'Importing RecipeItems'
+    puts 'Importing RecipeItems'
     Importer.new(
       bakery: bakery,
       collection: RecipeItems.all,
       importer: RecipeItemImporter
     )
       .import!
-      .tap { |c| Rails.logger.warn "Imported #{c.count} RecipeItems" }
+      .tap { |c| puts "Imported #{c.count} RecipeItems" }
   end
 
   def self.import_products
-    Rails.logger.warn 'Importing Products'
+    puts 'Importing Products'
     Importer.new(
       bakery: bakery,
       collection: Products.all,
       importer: ProductImporter
     )
       .import!
-      .tap { |c| Rails.logger.warn "Imported #{c.count} Products" }
+      .tap { |c| puts "Imported #{c.count} Products" }
   end
 
   def self.import_price_varients
-    Rails.logger.warn 'Importing PriceVarients'
+    puts 'Importing PriceVarients'
     Importer.new(
       bakery: bakery,
       collection: PriceVarients.all,
       importer: PriceVarientImporter
     )
       .import!
-      .tap { |c| Rails.logger.warn "Imported #{c.count} PriceVarients" }
+      .tap { |c| puts "Imported #{c.count} PriceVarients" }
   end
 
   def self.import_routes
-    Rails.logger.warn 'Importing Routes'
+    puts 'Importing Routes'
     Importer.new(
       bakery: bakery,
       collection: Routes.all,
       importer: RouteImporter
     )
       .import!
-      .tap { |c| Rails.logger.warn "Imported #{c.count} Routes" }
+      .tap { |c| puts "Imported #{c.count} Routes" }
   end
 
   def self.import_orders
-    Rails.logger.warn 'Importing Orders'
+    puts 'Importing Orders'
     Importer.new(
       bakery: bakery,
       collection: Orders.all.to_a,
       importer: OrderImporter
     )
       .import!
-      .tap { |c| Rails.logger.warn "Imported #{c.count} Orders" }
+      .tap { |c| puts "Imported #{c.count} Orders" }
   end
 
   def self.import_all
