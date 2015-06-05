8e2f2fafa70238595e0f6a4506bc9b9aada5a856
Francis Gulotta <wizard@roborooter.com>
2015-06-05 00:03:32 -0400

    properly set the redis url

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,4 +1,2 @@
 Resque.inline = ENV.key?('INLINE_JOBS')
-Resque.configure do |config|
-  config.redis = ENV['REDISCLOUD_URL'] if ENV['REDISCLOUD_URL']
-end
+Resque.redis = ENV['REDISCLOUD_URL'] if ENV['REDISCLOUD_URL']
