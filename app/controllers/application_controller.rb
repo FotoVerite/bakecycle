0ec35cf7d86768e945bc0593d0e35f2392aefff5
Francis Gulotta <wizard@roborooter.com>
2015-06-08 20:53:28 -0400

    Display a flash if resque isn’t setup in dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :active_nav
+  before_action :resque_no_worker if Rails.env.development?
   helper_method :current_bakery
   helper_method :item_finder
 
@@ -30,5 +31,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(_resource)
     root_path
+  end
+
+  def resque_no_worker
+    return if Resque.info[:workers] > 0 || Resque.inline
+    flash.alert = 'There are no resque workers and Resque.inline is false'
   end
 end
