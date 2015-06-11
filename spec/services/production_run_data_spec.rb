234e0d006b2b6ed37b78afda8af22b3f4205c058
Amy Gimma <amyg225@gmail.com>
2015-06-11 16:38:32 -0400

    tests

--- spec/services/production_run_data_spec.rb
+++ spec/services/production_run_data_spec.rb
@@ -16,4 +16,40 @@ describe ProductionRunData do
       expect(data.recipes_collection.count).to eq(2)
     end
   end
+
+  describe '#set_preferment_bowls' do
+    it 'sets preferment bowl counts that are in one mother dough' do
+      parent_recipes_array = [instance_double('RecipeRunData')]
+      preferment_recipe_data = mock_recipe_run_preferment(parent_recipes_array)
+
+      allow_any_instance_of(ProductionRunData).to receive(:preferments).and_return([preferment_recipe_data])
+      allow_any_instance_of(RecipeCollection).to receive(:detect).and_return(mock_parent_recipe)
+      expect(preferment_recipe_data).to receive(:mix_bowl_count=).with(3)
+      ProductionRunData.new(ProductionRun.new)
+    end
+
+    it 'does not set preferment bowl counts that are in more than one mother dough' do
+      parent_recipes_array = [instance_double('RecipeRunData'), instance_double('RecipeRunData')]
+      preferment_recipe_data = mock_recipe_run_preferment(parent_recipes_array)
+
+      allow_any_instance_of(ProductionRunData).to receive(:preferments).and_return([preferment_recipe_data])
+      allow_any_instance_of(RecipeCollection).to receive(:detect).and_return(mock_parent_recipe)
+      expect(preferment_recipe_data).not_to receive(:mix_bowl_count=)
+      ProductionRunData.new(ProductionRun.new)
+    end
+  end
+
+  def mock_recipe_run_preferment(parent_recipes_array)
+    instance_double('RecipeRunData',
+      mix_bowl_count: 1,
+      parent_recipes: parent_recipes_array
+                   )
+  end
+
+  def mock_parent_recipe
+    instance_double('RecipeRunData',
+      mix_bowl_count: 3,
+      parent_recipes: []
+                   )
+  end
 end
