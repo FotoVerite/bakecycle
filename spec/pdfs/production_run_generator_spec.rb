1b868dc4d48cd82646fa6f99c1cc54d4af0df2f0
Amy Gimma <amyg225@gmail.com>
2015-06-15 16:28:29 -0400

    production run filenames

--- spec/pdfs/production_run_generator_spec.rb
+++ spec/pdfs/production_run_generator_spec.rb
@@ -4,7 +4,7 @@ describe ProductionRunGenerator do
   it 'has a shape' do
     production_run = create(:run_item).production_run
     generator = ProductionRunGenerator.new(production_run)
-    expect(generator.filename).to eq('ProductionRunRecipe.pdf')
+    expect(generator.filename).to eq("Production-Run-#{production_run.id}-#{production_run.date}.pdf")
     expect(generator.generate).to_not be_nil
   end
 end
