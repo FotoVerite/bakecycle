36b28759d82804a5f3eb5bbcb1df4c4d2788d045
Armando Amador <arm.amador@gmail.com>
2015-07-06 15:47:12 -0400

    Create plans url page
     - Use plans partial from homepage to show on plans page
     - update homepage colors
     - remove `bc-darkgrey`
     - add plans to route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get :sign_in, to: 'landing_pages#sign_in'
   get 'privacy_policy', to: 'landing_pages#privacy_policy'
   get 'terms_of_service', to: 'landing_pages#terms_of_service'
+  get 'plans', to: 'landing_pages#plans'
   get :dashboard, to: 'dashboard#index'
 
   resources :registrations, only: [:new, :create]
