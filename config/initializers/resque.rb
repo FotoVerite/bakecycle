0b4fb1c556674c98dfaaf1048e2e76e0f55a3cab
Francis Gulotta <wizard@roborooter.com>
2015-07-08 17:32:26 -0400

    Quiet resque logging in tests (still logs to file)

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,8 +1,7 @@
 require 'resque/failure/airbrake'
 Resque::Failure.backend = Resque::Failure::Airbrake
-
 ActiveJob::Base.queue_adapter = :resque
 Resque.inline = ENV['INLINE_JOBS'] == 'true'
 Resque.redis = ENV['REDISCLOUD_URL'] if ENV['REDISCLOUD_URL']
-Resque.logger = MonoLogger.new(STDOUT)
+Resque.logger = MonoLogger.new(STDOUT) unless Rails.env.test?
 Resque.logger.level = MonoLogger::INFO
