7703870a383b0f64eea2cef330f928b53bc22519
Francis Gulotta <wizard@roborooter.com>
2015-08-05 11:26:27 -0400

    switch to a generic redis url

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,6 +2,6 @@ require "resque/failure/airbrake"
 Resque::Failure.backend = Resque::Failure::Airbrake
 ActiveJob::Base.queue_adapter = :resque
 Resque.inline = ENV["INLINE_JOBS"] == "true"
-Resque.redis = ENV["REDISCLOUD_URL"] if ENV["REDISCLOUD_URL"]
+Resque.redis = ENV["REDIS_URL"] if ENV["REDIS_URL"]
 Resque.logger = MonoLogger.new(STDOUT) unless Rails.env.test?
 Resque.logger.level = MonoLogger::INFO
