02e4828e8b4a3f2fa50e7667c197673d458508aa
Francis Gulotta <wizard@roborooter.com>
2016-04-14 23:49:26 -0400

    Show notes on orders

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
   attributes :id, :start_date, :end_date, :start_date, :order_type,
-    :errors, :client_id, :route_id, :total_lead_days
+    :errors, :client_id, :route_id, :total_lead_days, :note
   has_many :order_items
 end
