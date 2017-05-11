420cfc97ac1d96580fb5d9863b519e2f45bc9344
FotoVerite <mzbphoto@gmail.com>
2017-05-11 15:33:22 -0400

    forces s3 to make uploads downloads.

--- app/models/file_export.rb
+++ app/models/file_export.rb
@@ -16,6 +16,7 @@
 class FileExport < ActiveRecord::Base
   belongs_to :bakery
   has_attached_file :file,
+    s3_header: {"Content-Disposition" => "attachment"},
     use_timestamp: false,
     default_url: "",
     url: "/system/:class/:id/:attachment/:filename"
