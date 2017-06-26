96e7183dafbe87a8277d7ad0d14da781306995a3
FotoVerite <mzbphoto@gmail.com>
2017-06-26 14:38:22 -0400

    using date not start date.

--- app/reports/production_run_totals_generator.rb
+++ app/reports/production_run_totals_generator.rb
@@ -4,26 +4,26 @@ class ProductionRunTotalsGenerator
   def self.find(global_id)
     bakery_id, date_string, type = global_id.split("_")
     bakery = Bakery.find(bakery_id)
-    start_date = Date.iso8601(date_string)
+    date = Date.iso8601(date_string)
     type = type
-    new(bakery, start_date, type)
+    new(bakery, date, type)
   end
 
-  def initialize(bakery, start_date, type)
+  def initialize(bakery, date, type)
     @bakery = bakery
-    @start_date = start_date.to_date
+    @date = date.to_date
     @type = type
   end
 
   def id
-    "#{@bakery.id}_#{@start_date.iso8601}_#{@type}"
+    "#{@bakery.id}_#{@date.iso8601}_#{@type}"
   end
 
   def filename
     if @type == "daily"
-      "DailyProductionTotalsReport-#{@start_date.iso8601}.xlsx"
+      "DailyProductionTotalsReport-#{@date.iso8601}.xlsx"
     else
-      "WeeklyProductionTotalsReport-#{@start_date.iso8601}.xlsx"
+      "WeeklyProductionTotalsReport-#{@date.iso8601}.xlsx"
     end
   end
 
