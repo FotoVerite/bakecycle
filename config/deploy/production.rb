fbd1b08eac5da8365e74e43dfb071fefee389c21
FotoVerite <mzbphoto@gmail.com>
2018-01-28 19:58:30 -0500

    updates gemlock.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :deploy_to, "/var/www/bakecycle_production"
 set :user, "deploy"
-set :branch, "linode"
+set :branch, "rails5"
 set :ssh_options, forward_agent: true, port: 21500
 set :workers, "*" => 2
 set :resque_environment_task, true
