b538b9eaa0c79a0b09f9afc2477a59d7d6949901
FotoVerite <mzbphoto@gmail.com>
2018-02-06 03:41:52 -0500

    I give up put the socks in the tmp directory.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,10 +4,8 @@ set :branch, "rails5"
 set :ssh_options, forward_agent: true, port: 21500
 set :workers, "*" => 2
 set :resque_environment_task, true
+
 set :puma_conf, "#{current_path}/config/puma.rb"
-set :puma_state, "#{shared_path}/pids/puma.state"
-set :puma_pid, "#{shared_path}/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/sockets/puma.sock"    #accept array for multi-bind
 
 # Simple Role Syntax
 # ==================
@@ -19,3 +17,6 @@ role :web, %w[deploy@172.104.31.237]
 role :db,  %w[deploy@172.104.31.237]
 role :resque_worker, %w[deploy@172.104.31.237]
 role :resque_scheduler, %w[deploy@172.104.31.237]
+
+
+after "deploy:restart", "resque:restart"
