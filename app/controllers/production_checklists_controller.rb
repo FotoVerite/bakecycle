2940dc34762bc28d960fe362e45f5a256a90c6d1
FotoVerite <mzbphoto@gmail.com>
2018-05-02 15:59:43 -0400

    rubocop changes

--- app/controllers/production_checklists_controller.rb
+++ app/controllers/production_checklists_controller.rb
@@ -1,20 +1,18 @@
 class ProductionChecklistsController < ApplicationController
+  before_action :skip_authorization, :skip_policy_scope
 
-   before_action :skip_authorization, :skip_policy_scope
-
-    def show
-      @missing_shipments = policy_scope(Order).production_date(Time.zone.today)
+  def show
+    @missing_shipments = policy_scope(Order).production_date(Time.zone.today)
       .reject(&:no_outstanding_shipments?)
-      @double_invoices = Shipment.where(
-      "bakery_id = ? AND date between ? and ? ",
-      current_bakery,
-      (Time.zone.today - 2.days),
-      (Time.zone.today + 3.days)
+    @double_invoices = Shipment.where(
+    "bakery_id = ? AND date between ? and ? ",
+    current_bakery,
+    (Time.zone.today - 2.days),
+    (Time.zone.today + 3.days)
     )
       .group_by { |e| [e.date, e.client_id, e.route_id] }
       .select { |_k, v| v.size > 1 }.values.flatten
-      @production_run = current_bakery.production_runs.find_by(date: Time.zone.today)
-      @missing_items = current_bakery.shipment_items.where(production_start: Time.zone.today, production_run_id: nil) 
-    end
-
+    @production_run = current_bakery.production_runs.find_by(date: Time.zone.today)
+    @missing_items = current_bakery.shipment_items.where(production_start: Time.zone.today, production_run_id: nil)
+  end
 end
