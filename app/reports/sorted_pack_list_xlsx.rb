3e9ca5ac47ff9dea38420d882a3e79d3612c5cfd
FotoVerite <mzbphoto@gmail.com>
2018-08-31 01:35:00 -0400

    updates sorted pack list.

--- app/reports/sorted_pack_list_xlsx.rb
+++ app/reports/sorted_pack_list_xlsx.rb
@@ -9,12 +9,13 @@ class SortedPackListXlsx
     p = Axlsx::Package.new
     wb = p.workbook
     styles = wb.styles
-    @header = styles.add_style sz: 16, b: true, alignment: { horizontal: :left }
+    @header = styles.add_style sz: 15, b: true, alignment: { horizontal: :left }
     @rows = styles.add_style sz: 14, b: false, alignment: { horizontal: :left }
     @grouped_shipments.each do |shipments|
       wb.add_worksheet(name: "#{@date} #{shipments[0]}") do |sheet|
         hash = create_hash_of_products(shipments[1])
         sheet.add_row ["Pack List - #{@date} #{shipments[0]}"]
+        sheet.add_row []
         add_rows(hash, sheet)
       end
     end
@@ -30,10 +31,13 @@ class SortedPackListXlsx
           product_hash = hash[i.product_name] = {}
           product_hash["product_type"] = i.product.attributes["product_type"]
           product_hash["name"] = i.product_name
+          product_hash["quantity"] = {}
           product_hash["total"] = 0
         else
           product_hash = hash[i.product_name]
         end
+        product_hash["quantity"][client_name.to_s] = 0 if product_hash["quantity"][client_name.to_s].nil?
+        product_hash["quantity"][client_name.to_s] += i.product_quantity
         product_hash["total"] = (product_hash["total"].to_i + i.product_quantity)
       end
     end
@@ -46,7 +50,10 @@ class SortedPackListXlsx
     # Set Product Type Row
     hash.each do |product_array|
       sheet.add_row [product_array[0]], style: @header
-      sheet.add_row ["Total: " + product_array[1]["total"].to_s], style: @rows
+      product_array[1]["quantity"].each do |client_name, quantity|
+        sheet.add_row ["#{client_name} #{quantity}"]
+      end
+      sheet.add_row ["Total: " + product_array[1]["total"].to_s]
       sheet.add_row []
     end
   end
