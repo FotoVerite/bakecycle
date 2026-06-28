80a6f306f810de1a00b14be3501c893e6671bd90
Matthew Bergman <mzbphoto@gmail.com>
2026-06-28 03:14:42 -0400

    Update paperclip.rb

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -7,13 +7,17 @@ Paperclip.options[:content_type_mappings] = {
 aws_s3_bucket = ENV["AWS_S3_BUCKET"].presence || Rails.application.credentials.dig(:aws, :bucket)
 aws_s3_id = ENV["AWS_S3_ID"].presence || Rails.application.credentials.dig(:aws, :access_key_id)
 aws_s3_key = ENV["AWS_S3_KEY"].presence || Rails.application.credentials.dig(:aws, :secret_access_key)
+aws_s3_region = ENV["AWS_S3_REGION"].presence || ENV["AWS_REGION"].presence ||
+                Rails.application.credentials.dig(:aws, :region).presence || "us-east-1"
 
 if aws_s3_bucket.present? && aws_s3_id.present? && aws_s3_key.present?
   Paperclip::Attachment.default_options[:storage] = :s3
   Paperclip::Attachment.default_options[:s3_credentials] = {
       bucket: aws_s3_bucket,
       access_key_id: aws_s3_id,
-      secret_access_key: aws_s3_key
+      secret_access_key: aws_s3_key,
+      s3_region: aws_s3_region
     }
+  Paperclip::Attachment.default_options[:s3_region] = aws_s3_region
   Paperclip::Attachment.default_options[:s3_protocol] = :https
 end
