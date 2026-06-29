d1b8baa0b2bcaf21bad3a405fe092a2022499fa8
Matthew Bergman <mzbphoto@gmail.com>
2026-06-29 15:11:13 -0400

    Update for staging

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,8 +5,10 @@ Paperclip.options[:content_type_mappings] = {
 }
 
 aws_s3_bucket = ENV["AWS_S3_BUCKET"].presence || Rails.application.credentials.dig(:aws, :bucket)
-aws_s3_id = ENV["AWS_S3_ID"].presence || Rails.application.credentials.dig(:aws, :access_key_id)
-aws_s3_key = ENV["AWS_S3_KEY"].presence || Rails.application.credentials.dig(:aws, :secret_access_key)
+aws_s3_id = ENV["AWS_S3_ID"].presence || ENV["AWS_ACCESS_KEY_ID"].presence ||
+            Rails.application.credentials.dig(:aws, :access_key_id)
+aws_s3_key = ENV["AWS_S3_KEY"].presence || ENV["AWS_SECRET_ACCESS_KEY"].presence ||
+             Rails.application.credentials.dig(:aws, :secret_access_key)
 aws_s3_region = ENV["AWS_S3_REGION"].presence || ENV["AWS_REGION"].presence ||
                 Rails.application.credentials.dig(:aws, :region).presence || "us-east-1"
 
