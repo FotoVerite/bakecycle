379f1d095bdba4e6541c8dbdb0415c1888e9a74e
Matthew Bergman <mzbphoto@gmail.com>
2026-07-05 11:45:06 -0400

    smarter instrumentation

--- app/reports/generator.rb
+++ app/reports/generator.rb
@@ -38,10 +38,20 @@ module Generator
 
   module Instrumentation
     def generate
-      return super unless defined?(Sentry) && Sentry.respond_to?(:with_child_span)
+      return super unless sentry_tracing_available?
 
       started_at = Time.current
       output = nil
+
+      transaction = current_sentry_transaction
+      if sentry_exporter_job_transaction?(transaction)
+        set_sentry_report_transaction_data(transaction, started_at: started_at)
+        output = super()
+        set_sentry_report_result_data(transaction, output: output, started_at: started_at)
+        return output
+      end
+
+      return generate_with_standalone_sentry_transaction(started_at) { super() } unless current_sentry_span
 
       Sentry.with_child_span(op: "report.generate", description: sentry_report_name) do |span|
         set_sentry_report_data(span, started_at: started_at) if span
@@ -54,8 +64,37 @@ module Generator
 
     private
 
+    def sentry_tracing_available?
+      defined?(Sentry) &&
+        Sentry.respond_to?(:initialized?) &&
+        Sentry.initialized? &&
+        Sentry.respond_to?(:with_child_span)
+    end
+
     def sentry_report_name
       self.class.name.delete_suffix("Generator").underscore.humanize
+    end
+
+    def sentry_report_transaction_name
+      "Report Generate / #{sentry_report_name}"
+    end
+
+    def current_sentry_scope
+      Sentry.get_current_scope if Sentry.respond_to?(:get_current_scope)
+    end
+
+    def current_sentry_span
+      scope = current_sentry_scope
+      scope.get_span if scope&.respond_to?(:get_span)
+    end
+
+    def current_sentry_transaction
+      scope = current_sentry_scope
+      scope.get_transaction if scope&.respond_to?(:get_transaction)
+    end
+
+    def sentry_exporter_job_transaction?(transaction)
+      transaction&.respond_to?(:name) && transaction.name == "ExporterJob"
     end
 
     def set_sentry_report_data(span, started_at:)
@@ -64,9 +103,42 @@ module Generator
       end
     end
 
+    def set_sentry_report_transaction_data(transaction, started_at:)
+      transaction.set_name(sentry_report_transaction_name, source: :task) if transaction.respond_to?(:set_name)
+      transaction.set_data("op", "report.generate") if transaction.respond_to?(:set_data)
+      set_sentry_report_data(transaction, started_at: started_at)
+    end
+
     def set_sentry_report_result_data(span, output:, started_at:)
       span.set_data("duration_ms", ((Time.current - started_at) * 1000).round(1))
       span.set_data("output_bytes", output.bytesize) if output.respond_to?(:bytesize)
+    end
+
+    def generate_with_standalone_sentry_transaction(started_at)
+      transaction = Sentry.start_transaction(
+        name: sentry_report_transaction_name,
+        source: :task,
+        op: "report.generate",
+        description: sentry_report_name
+      ) if Sentry.respond_to?(:start_transaction)
+
+      return yield unless transaction
+
+      output = nil
+
+      Sentry.with_scope do |scope|
+        scope.set_span(transaction) if scope.respond_to?(:set_span)
+        set_sentry_report_data(transaction, started_at: started_at)
+        output = yield
+        set_sentry_report_result_data(transaction, output: output, started_at: started_at)
+      rescue Exception # rubocop:disable Lint/RescueException
+        transaction.set_http_status(500) if transaction.respond_to?(:set_http_status)
+        raise
+      ensure
+        transaction.finish if transaction.respond_to?(:finish)
+      end
+
+      output
     end
 
     def sentry_base_report_attributes(started_at)
