904b1b4f6c1268ada86c4d0f8d269aeefa45aa14
Matthew Bergman <mzbphoto@gmail.com>
2026-07-14 12:05:04 -0400

    update nightly sign off.

--- spec/requests/nightly_sign_offs_spec.rb
+++ spec/requests/nightly_sign_offs_spec.rb
@@ -22,6 +22,9 @@ RSpec.describe "Nightly sign off", type: :request do
     expect(response.body).to include("Wholesale Sandwiches")
     expect(response.body).to include("Cafe Sandwich")
     expect(response.body).to include("1 invoice")
+    expect(response.body).to include("Packers")
+    expect(response.body).to include("Drivers")
+    expect(response.body).to include('class="col-drivers"')
   end
 
   it "does not list Internal-channel clients in the Wholesale Sandwiches section" do
